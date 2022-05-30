14af224fcb49d6b92b0b08f0f198891b61196c39
yadon079 <zeidgame@gmail.com>
2022-05-30 21:41:38 +0900

    feature : Bean Validation 한계
    <description>
    * 검증
    - Bean Validation 한계
    >> 등록과 수정 둘 다 만족시키지 못하는 경우가 존재 -> groups 사용
    
    <issue-number>

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,17 +11,19 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class)    // 수정 요구사항 추가
     private Long id;
 
-    @NotBlank(message = "공백은 입력할 수 없습니다.")
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 100000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 100000,
+        groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
